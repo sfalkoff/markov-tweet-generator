e44eabaedf1950d7a6bce3bc936689fe1df807c3
Hackbright Student <info@hackbrightacademy.com>
2015-01-21 17:15:09 -0800

    First commit

---
+++ markov.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+
+def make_chains(corpus):
+    """Takes an input text as a string and returns a dictionary of
+    markov chains."""
+    #text to open and read
+    text_file = open(corpus)
+    text_file = text_file.read().split()
+    print text_file
+    for word in range(len(text_file) - 2):
+        print text_file[word]
+    #remove white space at end of lines
+    #split the text into a list of words
+
+
+
+    #make a dictrionary
+    #return chains_dict
+    #dictionary will contain tuples with two words as keys and the value will be a list
+    #building the data structure 
+    # return {}
+
+def make_text(chains):
+    """Takes a dictionary of markov chains and returns random text
+    based off an original text."""
+    #using the above data structure to generate random text
+    return "Here's some random text."
+
+def main():
+    args = sys.argv
+
+
+    #allows user to pass a text file to the program in the command line
+
+    # Change this to read input_text from a file
+    input_text =  args[1]
+
+    chain_dict = make_chains(input_text)
+    random_text = make_text(chain_dict)
+    # print random_text
+    print make_chains
+
+if __name__ == "__main__":
+    main()
